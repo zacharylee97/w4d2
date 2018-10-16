19c878c496a5c417ef5eb7ce3178ef83804aaced
Zachary Lee <zacharylhlee@gmail.com>
2018-10-16 17:01:10 +0000

    Adds new migration add_famous_person_id_to_milestones

---
+++ connecting-from-js/migrations/20181016164441_add_famous_person_id_to_milestones.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function(table) {
+      table.integer('famous_person_id').references('id').inTable('famous_people');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function(table) {
+      table.dropColumn('famous_person_id');
+    })
+  ])
+};
