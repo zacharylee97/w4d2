14bf5025f967ca1e4c0389855f7cf10f39d6e81c
Zachary Lee <zacharylhlee@gmail.com>
2018-10-16 15:15:15 +0000

    Format result output

--- connecting-from-js/famous_people.js
+++ connecting-from-js/famous_people.js
@@ -10,19 +10,30 @@ const client = new pg.Client({
   ssl: settings.ssl
 });
 
-const name = process.argv[2];
-if (name) {
+const query = process.argv[2];
+if (query) {
   console.log("Searching...");
   client.connect((err) => {
     if (err) {
       return console.error("Connection Error", err);
     }
-    //Search for matching first name
-    client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [name], (err, result) => {
+    //Search for match with first_name or last_name
+    client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [query], (err, result) => {
       if (err) {
         return console.err("error running query", err);
       }
-      console.log(result.rows);
+      const numOfResults = result.rows.length;
+      console.log(`Found ${numOfResults} person(s) by the name '${query}'`)
+      let counter = 1;
+      //Format results
+      result.rows.forEach(function(element) {
+        const firstName = element['first_name'];
+        const lastName = element['last_name'];
+        const DOB = element['birthdate'].toISOString().substring(0,10);
+        console.log(`- ${counter}: ${firstName} ${lastName}, born '${DOB}'`);
+        counter++;
+      });
+
     client.end();
     });
   });
