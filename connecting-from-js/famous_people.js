d1f0721f1ddf7728bc8863daed6208978eca7337
Zachary Lee <zacharylhlee@gmail.com>
2018-10-16 14:59:39 +0000

    Adds lookup script for famous_people

---
+++ connecting-from-js/famous_people.js
@@ -0,0 +1,27 @@
+const pg = require("pg");
+const settings = require("./settings");
+
+const client = new pg.Client({
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.host,
+  port: settings.port,
+  ssl: settings.ssl
+});
+
+const name = process.argv[2];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  //Search for matching first name
+  client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [name], (err, result) => {
+    if (err) {
+      return console.err("error running query", err);
+    }
+    console.log(result.rows);
+  client.end();
+  });
+});
