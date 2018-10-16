1b0d0135e4a2d11468a069b3e64a148e74381222
Zachary Lee <zacharylhlee@gmail.com>
2018-10-16 15:23:04 +0000

    Refactor code

--- connecting-from-js/famous_people.js
+++ connecting-from-js/famous_people.js
@@ -10,33 +10,48 @@ const client = new pg.Client({
   ssl: settings.ssl
 });
 
-const query = process.argv[2];
-if (query) {
-  console.log("Searching...");
-  client.connect((err) => {
-    if (err) {
-      return console.error("Connection Error", err);
-    }
-    //Search for match with first_name or last_name
-    client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [query], (err, result) => {
+function searchForName(query) {
+  if (query) {
+    //Connect to database
+    console.log("Searching...");
+    client.connect((err) => {
       if (err) {
-        return console.err("error running query", err);
+        return console.error("Connection Error", err);
       }
-      const numOfResults = result.rows.length;
-      console.log(`Found ${numOfResults} person(s) by the name '${query}'`)
-      let counter = 1;
-      //Format results
-      result.rows.forEach(function(element) {
-        const firstName = element['first_name'];
-        const lastName = element['last_name'];
-        const DOB = element['birthdate'].toISOString().substring(0,10);
-        console.log(`- ${counter}: ${firstName} ${lastName}, born '${DOB}'`);
-        counter++;
+      //Search for match with first_name or last_name
+      client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [query], (err, result) => {
+        if (err) {
+          return console.err("error running query", err);
+        }
+        //Format results
+        const output = formatResults(result.rows);
+        console.log(output);
+      client.end();
       });
-
-    client.end();
     });
-  });
-} else {
-  console.log("Please enter a name!");
+  } else {
+    console.log("Please enter a name!");
+  }
 }
+
+function formatResults(results) {
+  let output = "";
+  const numOfResults = results.length;
+  output += `Found ${numOfResults} person(s) by the name '${query}'\n`;
+  let counter = 1;
+  results.forEach(function(element, index) {
+    const firstName = element['first_name'];
+    const lastName = element['last_name'];
+    const DOB = element['birthdate'].toISOString().substring(0,10);
+    output += `- ${counter}: ${firstName} ${lastName}, born '${DOB}'`;
+    if (index != numOfResults - 1) {
+      output += "\n";
+    }
+    counter++;
+  });
+  return output;
+}
+
+
+const query = process.argv[2];
+searchForName(query);
